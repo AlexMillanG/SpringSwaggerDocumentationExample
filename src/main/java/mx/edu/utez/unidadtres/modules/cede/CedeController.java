778b0678be8a4a484e5db357d792013b1a77563a
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-07-01 15:51:30 -0600

    [UPDATE] cede controller mapping

---
+++ src/main/java/mx/edu/utez/unidadtres/modules/cede/CedeController.java
@@ -0,0 +1,26 @@
+package mx.edu.utez.unidadtres.modules.cede;
+
+import mx.edu.utez.unidadtres.utils.APIResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/cede")
+@RestController
+public class CedeController {
+
+    @Autowired
+    private CedeService cedeService;
+
+    @GetMapping("")
+    public ResponseEntity<APIResponse> findAll (){
+        APIResponse response = cedeService.findAll();
+        return new ResponseEntity<>(response,response.getStatus());
+    }
+
+    @PostMapping("")
+    public ResponseEntity<APIResponse> save (@RequestBody CedeEntity cede){
+        APIResponse response = cedeService.save(cede);
+        return new ResponseEntity<>(response,response.getStatus());
+    }
+}
