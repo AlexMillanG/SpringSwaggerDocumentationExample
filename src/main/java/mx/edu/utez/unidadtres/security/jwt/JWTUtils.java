ea347c05c511d25187833fde253b343caa929fd3
AlexMillanG <132949276+AlexMillanG@users.noreply.github.com>
2025-07-08 15:46:35 -0600

    [UPDATE] somes changes

--- src/main/java/mx/edu/utez/unidadtres/security/jwt/JWTUtils.java
+++ src/main/java/mx/edu/utez/unidadtres/security/jwt/JWTUtils.java
@@ -1,4 +1,40 @@
 package mx.edu.utez.unidadtres.security.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.function.Function;
+
+//CUARTO PASO:
+@Service
 public class JWTUtils {
+
+    @Value("${secret.key}")
+    private String SECRET_KEY;
+
+    //extrae todo las cosas del fakin token
+    //body, headers,
+    public Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY) //desencripta con la llave secrete
+                .parseClaimsJws(token)//parsea el cuerpo del token
+                .getBody();//obtiene el cuerpo del token
+    }
+
+    //extrae las propiedades del token
+    private <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+    final Claims CLAIMS = extractAllClaims(token);
+    return claimsResolver.apply(CLAIMS);
+    }
+
+    public String extractUsername(String token){
+        return  extractClaim(token,Claims::getSubject);
+    }
+
+    public Date extractExpirationDate(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
 }
--- src/main/java/mx/edu/utez/unidadtres/security/jwt/UDService.java
+++ src/main/java/mx/edu/utez/unidadtres/security/jwt/UDService.java
@@ -1,7 +1,35 @@
 package mx.edu.utez.unidadtres.security.jwt;
 
+import mx.edu.utez.unidadtres.modules.user.BeanUser;
+import mx.edu.utez.unidadtres.modules.user.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+
 @Service
-public class UserDetailsService {
+public class UDService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        BeanUser found = userRepository.findByUsername(username).orElse(null);
+        if (found== null) throw new UsernameNotFoundException("usuario no encontrado");
+
+        //authority = ROLE_ADMIN,  ROLE_EMPLOYEE
+        GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" +found.getRol().getName());
+
+
+        return new User(
+                found.getUsername(),
+                found.getPassword(),
+                Collections.singleton(authority)
+        );
+    }
 }
